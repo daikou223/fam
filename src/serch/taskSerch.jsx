336ae4fcb0ceca78700f72cde166ed7bd4fb4d33
daikou1928 <daikou1928@gmail.com>
2025-05-25 12:05:05 +0900

    [feat] 検索機能の表示を実装

--- src/serch/taskSerch.jsx
+++ src/serch/taskSerch.jsx
@@ -1,9 +1,10 @@
-import style from "../style.css"
 import { useState,useEffect,useRef} from "react";
+import dayjs from 'dayjs';
 
 function Serch(){
     //変数定義**********************
     let lowData = JSON.parse(localStorage.getItem("task"));
+    let id = localStorage.getItem("id");
     const taskName = useRef("");
     const [dispText,setDispText] = useState("");
     const [filterdData,setFilterdData] = useState([]);
@@ -13,14 +14,14 @@ function Serch(){
         let filterdData_ = [];
         for(let i = 0;i<lowData.length;i++){
             let isPermit = true; 
-            if(!(lowData[i].taskname.includes(TaskName_))){
+            if(!(lowData[i].taskname.includes(TaskName_)) || lowData[i].user_id != id){
                 isPermit = false;
             }
             if(isPermit){
                 filterdData_.push(lowData[i]);
             }
         }
-        console.log(filterdData_);
+        console.log(filterdData_)
         setFilterdData(filterdData_);
     }
     return(
@@ -35,8 +36,31 @@ function Serch(){
             onClick = {()=>serchTask()}>
             検索
             </button>
+        <div style = {styles.serResult}>
+            <div>検索結果：{filterdData.length}件</div>
+        {filterdData.map((task)=>{
+            const dispDate = dayjs(task.date.slice(0,11)).format('YYYY/MM/DD');
+            return(<div style = {styles.oneResult}>
+                <div style = {styles.title}>{task.taskname}</div><>{dispDate}</>
+            </div>)
+        })}
+        </div>
         </div>
     )
+}
+
+const styles = {
+    serResult:{
+        border:"2px solid black",
+        borderRadius:"5px" 
+    },
+    oneResult:{
+        borderTop:"1px solid lightgray",
+        padding:"5px 10px",
+    },
+    title:{
+        fontSize:"10px",
+    }
 }
 
 export default Serch;
