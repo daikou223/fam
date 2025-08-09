b6c29e241c9cd8aa33ae2ec114d06bffccd32fd6
daikou1928 <daikou1928@gmail.com>
2025-08-10 02:58:06 +0900

    開発環境を整える

--- src/api/TaskApi.js
+++ src/api/TaskApi.js
@@ -2,35 +2,67 @@ import axios from "axios";
 import dayjs from "dayjs";
 //タスクのアップデート
 export async function update(name,goto,start,end,memo,ids,isHome){
-    await axios.put(
-        'https://fam-api-psi.vercel.app/api/tasks',
-        {
-            taskname:name,
-            forgoto:goto,
-            start:start,
-            end:end,
-            memo:memo,
-            taskids:ids,
-            isHome:isHome
-        }
-    ).then(
-        response=>{
-            console.log('post成功');
-            return response
-        }
-    )
+    if(!localStorage.getItem("debug")){
+        await axios.put(
+            'https://fam-api-psi.vercel.app/api/tasks',
+            {
+                taskname:name,
+                forgoto:goto,
+                start:start,
+                end:end,
+                memo:memo,
+                taskids:ids,
+                isHome:isHome
+            }
+        ).then(
+            response=>{
+                console.log('post成功');
+                return response
+            }
+        )
+    }else{
+        await axios.put(
+            'https://fam-api-psi.vercel.app/api/debug/tasks',
+            {
+                taskname:name,
+                forgoto:goto,
+                start:start,
+                end:end,
+                memo:memo,
+                taskids:ids,
+                isHome:isHome
+            }
+        ).then(
+            response=>{
+                console.log('post成功');
+                return response
+            }
+        )
+    }
 }
 //削除Api
 export async function dltApi(ids){
-    await axios.delete(
-        `https://fam-api-psi.vercel.app/api/tasks/${ids.join(",")}`
-    ).then(
-        response=>{
-            console.log('delete成功');
-            localStorage.removeItem("task")
-            return response
-        }
-    )
+    if(!localStorage.getItem("debug")){
+        await axios.delete(
+            `https://fam-api-psi.vercel.app/api/tasks/${ids.join(",")}`
+        ).then(
+            response=>{
+                console.log('delete成功');
+                localStorage.removeItem("task")
+                return response
+            }
+        )
+    }else{
+        await axios.delete(
+            `https://fam-api-psi.vercel.app/api/debug/tasks/${ids.join(",")}`
+        ).then(
+            response=>{
+                console.log('delete成功');
+                localStorage.removeItem("task")
+                return response
+            }
+        )
+    }
 }
 //タスクの取得
 export async function getTask(mode = ""){
@@ -38,29 +70,57 @@ export async function getTask(mode = ""){
         let result = JSON.parse(localStorage.getItem("task"));
         return result
     }
-    const response = await axios
-      .get(`https://fam-api-psi.vercel.app/api/tasks`)             //リクエストを飛ばすpath
-      .catch((error) => {
-        throw new Error('タスク取得エラー',error);
-    });  
-    localStorage.setItem("date",dayjs().toISOString())
-    localStorage.setItem("task",JSON.stringify(response.data))
-    return response.data
+    if(!localStorage.getItem("debug")){
+        const response = await axios
+        .get(`https://fam-api-psi.vercel.app/api/tasks`)             //リクエストを飛ばすpath
+        .catch((error) => {
+            throw new Error('タスク取得エラー',error);
+        });  
+        localStorage.setItem("date",dayjs().toISOString())
+        localStorage.setItem("task",JSON.stringify(response.data))
+        return response.data
+    }else{
+        const response = await axios
+        .get(`https://fam-api-psi.vercel.app/api/debug/tasks`)             //リクエストを飛ばすpath
+        .catch((error) => {
+            throw new Error('タスク取得エラー',error);
+        });  
+        localStorage.setItem("date",dayjs().toISOString())
+        localStorage.setItem("task",JSON.stringify(response.data))
+        return response.data
+    }
 }
 //タスクの登録
 export async function postTask(id,name,gototime,date,start,end,memo,home){
-    await axios.post(
-        'https://fam-api-psi.vercel.app/api/tasks',
-        {userid:id,
-        taskname:name,
-        forgoto:gototime+":00",
-        date:date,
-        start:start+":00",
-        end:end+":00",
-        memo:memo,
-        isHome:home
-        }
-    ).catch((error)=>{
-        throw new Error('タスク保存エラー',error)
-    })
+    if(!localStorage.getItem("debug")){
+        await axios.post(
+            'https://fam-api-psi.vercel.app/api/tasks',
+            {userid:id,
+            taskname:name,
+            forgoto:gototime+":00",
+            date:date,
+            start:start+":00",
+            end:end+":00",
+            memo:memo,
+            isHome:home
+            }
+        ).catch((error)=>{
+            throw new Error('タスク保存エラー',error)
+        })
+    }else{
+        await axios.post(
+            'https://fam-api-psi.vercel.app/api/debug/tasks',
+            {userid:id,
+            taskname:name,
+            forgoto:gototime+":00",
+            date:date,
+            start:start+":00",
+            end:end+":00",
+            memo:memo,
+            isHome:home
+            }
+        ).catch((error)=>{
+            throw new Error('タスク保存エラー',error)
+        })
+    }
 }
