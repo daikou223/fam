958025ae3b1a82135c030b7d847c9abb33b33065
daikou1928 <daikou1928@gmail.com>
2025-09-25 22:34:48 +0900

    localStrageをできるだけなくす

--- src/serch/taskSerch.jsx
+++ src/serch/taskSerch.jsx
@@ -1,21 +1,22 @@
 import { useState,useEffect,useRef} from "react";
 import dayjs from 'dayjs';
 import Menubar from "../menubar/menubar"
+import * as TaskUtil from "./../class/TaskClass"
 
 function Serch(){
     //変数定義**********************
-    let lowData = JSON.parse(localStorage.getItem("task"));
     let id = localStorage.getItem("id");
     const taskName = useRef("");
-    const [dispText,setDispText] = useState("");
     const [filterdData,setFilterdData] = useState([]);
     //関数定義************************
-    function serchTask(){
+    async function serchTask(){
+        let lowData = await TaskUtil.getTask();
+        lowData = lowData.reverse()
         const TaskName_ = taskName.current.value;
         let filterdData_ = [];
         for(let i = 0;i<lowData.length;i++){
             let isPermit = true; 
-            if(!(lowData[i].taskname.includes(TaskName_)) || lowData[i].user_id != id){
+            if(!(lowData[i].name.includes(TaskName_)) || lowData[i].user_id != id){
                 isPermit = false;
             }
             if(isPermit){
@@ -42,9 +43,9 @@ function Serch(){
         <div style = {styles.serResult}>
             <div>検索結果：{filterdData.length}件</div>
         {filterdData.map((task)=>{
-            const dispDate = dayjs(task.date.slice(0,11)).format('YYYY/MM/DD');
+            const dispDate = task.date.format('YYYY/MM/DD');
             return(<div style = {styles.oneResult}>
-                <div style = {styles.title}>{task.taskname}</div><>{dispDate}</>
+                <div style = {styles.title}>{task.name}</div><>{dispDate}</>
             </div>)
         })}
         </div>
